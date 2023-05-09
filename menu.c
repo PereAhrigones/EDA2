9f716518960d5084e496235015a87db243e46f00
PereAhrigones <albert.perez06@estudiant.upf.edu>
2023-05-09 14:05:13 +0200

    Lo del martes 9

--- menu.c
+++ menu.c
@@ -35,34 +35,68 @@ void  menu() {
 
     if(login == 2){
 
-        User_data user_search;
+        char nombre[MAX_PASSWORD_LENGTH];
+        char contraseña[MAX_PASSWORD_LENGTH];
         printf("Ingresa el nombre de usuario\n");
-        scanf("%s", user_search.username);
+        scanf("%s", nombre);
         printf("Ingresa la contraseña:\n");
-        scanf("%s", user_search.password);
-        buscar_usuario(user_search.username,user_search.password);
+        scanf("%s", contraseña);
+        buscar_usuario(nombre, contraseña);//Hay que juntar esto con lo de abajo y hacer un while por si se equivoca al iniciar sesión pueda volver a intentarlo
 
+        if (buscar_usuario(nombre, contraseña)) {
+            printf("Inicio de sesión exitoso. ¡Bienvenido!\n");
+
+            // Menú con opciones
+            int opcion;
+            int opcion2;
+            int edad;
+            char nombre_usuario;
+            char ubicacion;
+            char descripcion;
+
+            printf("\n----- Menú -----\n");
+            printf("1. Perfil (1)\n");
+            printf("2. Página principal (2)\n");
+            printf("0. Salir (0) \n");
+            printf("Ingresa la opción deseada: ");
+            scanf("%d", &opcion);
+
+            if (opcion == 0) {
+
+            }
+            if (opcion == 1) {
+                printf("\n----- Perfil -----\n");
+                printf("1. Editar (1)\n");
+                printf("2. Ver valoraciones\n");
+                printf("0. Atrás (0) \n");
+                printf("Ingresa la opción deseada: ");
+                scanf("%d", &opcion2);
+
+                if (opcion2 == 0) {
+
+                }
+                if (opcion2 == 1) {
+                    printf("Introduce tu nombre de usuario: \n");
+                    scanf("%d", &nombre_usuario);
+                    printf("Introduce tu Edad: \n");
+                    scanf("%d", &edad);
+                    printf("Introduce tu ubicación: \n");
+                    scanf("%d", &ubicacion);
+                    printf("Introduce una descripción:\n");
+                    printf("\n");
+                    scanf("%d", &descripcion);
+
+                }
+                if (opcion2 == 2) {
+
+                }
+
+            }
+
+            if (opcion == 2) {
+            }
+
+
+        }
     }
-
 }
-
-
-
-void create_user(){
-    
-    FILE* fp;
-    fp = fopen("C:\\Users\\senyo\\CLionProjects\\EDA2\\Usuarios", "w"); //Si queréis escribir en el archivo no lo podéis abriren modo lectura
-    while (fp != EOF){
-
-
-    }
-
-    fscanf(fp, "%s,%s\n", user_search->username, user_search->password); //Esto hay que retocarlo porque está medio meh.
-    fclose(fp);
-}
-
-
-}
-
-
-
--- menu.h
+++ menu.h
@@ -5,14 +5,20 @@
 #ifndef EDA2_MENU_H
 #define EDA2_MENU_H
 
-#define MAX_USENAME_LENGTH 40
+#define MAX_USERNAME_LENGTH 16
 #define MIN_USERNAME_LENGTH 4
-#define MAX_PASSWORD_LENGTH 20
+#define MAX_PASSWORD_LENGTH 40
 #define MIN_PASSWORD_LENGTH 8
-#define MAX_EMAIL_LENGTH 20
+#define MAX_EMAIL_LENGTH 60
+#define TRUE 1
+#define FALSE 0
+#define USERNAME_ALREADY_EXISTS -1
+#define USER_ALREADY_EXISTS -2
+#define USER_DOES_NOT_EXIST -3
+#define NO_FILE_FOUND 500
 
 typedef struct {
-    char username[MAX_USENAME_LENGTH];
+    char username[MAX_USERNAME_LENGTH];
     char email[MAX_EMAIL_LENGTH];
     char password[MAX_PASSWORD_LENGTH];
     int user_number;
@@ -22,7 +28,7 @@ typedef struct {
 User_data* user_declaration();
 
 void registrar_usuario();
-void buscar_usuario(int algo, int otro);// Tengo que mirar como estan declaradas las cosas en el otro proyecto
+int buscar_usuario(char algo[MAX_USERNAME_LENGTH], char otro[MAX_PASSWORD_LENGTH]);// Tengo que mirar como estan declaradas las cosas en el otro proyecto
 void create_user(); //Esto no va a ser void porque va a tener que devolver una estructura pero hasta que la creemos se queda así
 
 void menu();
